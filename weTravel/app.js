99dac5485caee17f096f7bc8f9dafd7515d3da5e
Chi.Jack <linjian2005@gmail.com>
2019-07-23 11:12:43 +0800

    微信小程序

--- weTravel/app.js
+++ weTravel/app.js
@@ -1,3 +1,8 @@
+/**
+ * Author : 丸子团队（波波、Chi、ONLINE.信）
+ * Github 地址: https://github.com/dchijack/Travel-Mini-Program
+ * GiTee 地址： https://gitee.com/izol/Travel-Mini-Program
+ */
 //app.js
 const API = require('/utils/base')
 
